f0757063e4c45da7af93821961a91a186cd50383
alanflomen <alanflomen@users.noreply.github.com>
2018-04-21 17:20:38 -0300

    jj

--- ProyectoT4/RelgasNegocio/Prueba.cs
+++ ProyectoT4/RelgasNegocio/Prueba.cs
@@ -18,10 +18,37 @@ namespace ProyectoT4.RelgasNegocio
             }
             else if (a.Equals('q'))
             {
-                usuario.Wishlist.Remove(idJuego);
+                eliminarJuego(idJuego, idUsuario, db, 'w');
             }
 
         }
+
+        private static void eliminarJuego(int idJuego, int idUsuario, sistemaContext db, char tipoLista)
+        {
+            try
+            {
+                //si son nulos que tire una expepcion con detalles
+                Usuario user = buscarUsuario(idUsuario, db);
+                Juego juego = BuscarJuego(idJuego, db);
+
+                //si no existe en la lista
+                if (user.buscarEnLista(idJuego, tipoLista))
+                {
+                    //actualiza esto la bd? o no es necesaria ahora la lista??
+                    user.Wishlist.Remove(idJuego);
+                }
+                else
+                {
+                    throw new Exception("El juego no esta en la lista!");
+                }
+
+            }
+            catch (Exception e)
+            {       //mostrar en algun lado el error
+                Console.WriteLine(e.Message);
+            }
+        }
+
         public static void agregarJuego(int idJuego, int idUsuario, sistemaContext db, char tipoLista)
         {
             try
