087640788cde458c8e0f2c5bd517ebe06335e765
nicobiyi <vicentini.nicolas@gmail.com>
2018-05-26 16:36:13 -0300

    Propuesta, validacion y Mensaje

---
+++ ProyectoT4/Controllers/Propuesta/PropuestaController.cs
@@ -0,0 +1,48 @@
+﻿using ProyectoT4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoT4.RelgasNegocio;
+
+namespace ProyectoT4.Controllers.Propuesta
+{
+    public class PropuestaController : Controller
+    {
+        // GET: Propuesta
+        public ActionResult RealizarPropuesta( int Idjuego1, int Idjuego2, string IdUsuario1,string IdUsuario2, string textoOpcional)
+        {
+			//Mensaje de resultado de Realizar propueta
+			string resPropuesta = "";
+
+			//Realizamos Validaciones en RN
+			ValidacionOperacion validacion = new ValidacionOperacion();
+			bool operacionValida = validacion.ValidarOperacion(Idjuego1, Idjuego2, IdUsuario1, IdUsuario2, textoOpcional);
+		
+
+			if (operacionValida)
+			{
+				//Contruir la Operacion
+
+				sistemaContext db = new sistemaContext();
+				Usuario jugador1 = db.Usuarios.Find(IdUsuario1);
+				Usuario jugador2 = db.Usuarios.Find(IdUsuario2);
+				Juego juego1 = db.Juegos.Find(Idjuego1);
+				Juego juego2 = db.Juegos.Find(Idjuego2);
+
+				var operacion = new Operacion( jugador1,  jugador2,  juego1,  juego2);
+
+				//Guardar en la DB
+
+				resPropuesta = "Propuesta Realizada";
+			}
+			else
+			{
+				resPropuesta = "Propuesta Invalida";
+			}
+
+            return View(resPropuesta);
+        }
+    }
+}
---
+++ ProyectoT4/RelgasNegocio/ValidacionOperacion.cs
@@ -0,0 +1,32 @@
+﻿using ProyectoT4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoT4.RelgasNegocio
+{
+	public class ValidacionOperacion
+	{
+		public bool ValidarOperacion(int Idjuego1, int Idjuego2, string IdUsuario1, string IdUsuario2, string textoOpcional)
+		{
+			bool OpValida = false;
+			//sistemaContext db = new sistemaContext();
+
+			//validar que usuario 1 aún tenga el juego1 en su Wishlist y el Juego2 en su biblioteca
+			
+
+			//validar que usuario 2 aún tenga el juego1 en su biblioteca y el Juego2 en su Wishlist
+			
+
+			//Validar que en el textoOpcional no se utilicen palabrotas
+			if (!textoOpcional.Contains("Puto"))
+			{
+				OpValida = true;
+			}
+			
+			return OpValida;
+		}
+
+	}
+}
