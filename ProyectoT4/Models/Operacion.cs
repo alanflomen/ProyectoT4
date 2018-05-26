31e2130ad0291b856932fb98548f8065e7b7c485
nicobiyi <vicentini.nicolas@gmail.com>
2018-05-26 13:34:34 -0300

    cambio en modelo Operacion

--- ProyectoT4/Models/Operacion.cs
+++ ProyectoT4/Models/Operacion.cs
@@ -12,19 +12,19 @@ namespace ProyectoT4.Models
         public DateTime Fecha { get; set; }
         public Juego[] Juegos { get; set; }
         //Tipo = Canje o Venta
-        public String Tipo { get; set; }
+        //public String Tipo { get; set; }
 
         //constructor parametrizado
-        Operacion(Usuario jugador1, Usuario jugador2, Juego juego1, Juego juego2, String tipo)
+        public Operacion(Usuario jugador1, Usuario jugador2, Juego juego1, Juego juego2)
         {
             this.Usuarios = new Usuario[] { jugador1, jugador2 };
             this.Fecha = DateTime.Now;
             this.Juegos = new Juego[] { juego1, juego2 };
-            this.Tipo = tipo;
-            this.IdOperacion = asignarId();
+            //this.Tipo = tipo;
+            this.IdOperacion = AsignarId();
         }
         //devuelve un valor del Id incrementado en 1, respecto al ultimo creado
-        private int asignarId()
+        private int AsignarId()
         {
             return 0;
         }
