c2c7bfefb4b654b53d46b861c54026aef50e0a5a
alanflomen <alanflomen@users.noreply.github.com>
2018-06-02 15:32:32 -0300

    agregado modelo de resultado busqueda SIN login

---
+++ ProyectoT4/AccesoDatos/ArmadorSinLogin.cs
@@ -0,0 +1,40 @@
+﻿using ProyectoT4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoT4.AccesoDatos
+{
+    public class ArmadorSinLogin
+    {
+        public static JuegosMatch SinLogin(int idJuego)
+        {
+            JuegosMatch jm;
+            var db = new sistemaContext();
+            Juego j = db.Juegos.Find(idJuego);
+            jm = new JuegosMatch(idJuego, j.Rating, j.Titulo);
+            List<String> usuarios = db.Libreria.Where(o => o.IdJuego == idJuego).Select(i => i.IdUsuario).ToList();
+
+            if (usuarios.Count != 0)
+            {
+                jm.UsuariosMatch = listaUsuarios(usuarios);
+            }
+
+            return jm;
+
+        }
+        public static List<Usuario> listaUsuarios(List<String> lista)
+        {
+            List<Usuario> listaUsuarios = new List<Usuario>();
+            var db = new sistemaContext();
+            Usuario u;
+            foreach (var us in lista)
+            {
+                u = db.Usuarios.Find(us);
+                listaUsuarios.Add(u);
+            }
+            return listaUsuarios;
+        }
+    }
+}
---
+++ ProyectoT4/Controllers/ResultadoSinLogin/ResultadoSinLoginController.cs
@@ -0,0 +1,20 @@
+﻿using ProyectoT4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoT4.Controllers
+{
+    public class ResultadoSinLoginController : Controller
+    {
+        // GET: ResultadoSinLogin
+        public ActionResult ResultadoSinLogin(int idJuego)
+        {
+            ViewBag.Title = "Resultado Sin Login";
+            JuegosMatch jm = AccesoDatos.ArmadorSinLogin.SinLogin(idJuego);
+            return View(jm);
+        }
+    }
+}
